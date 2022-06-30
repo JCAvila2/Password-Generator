3db3d1f98af54bcb9ddce3157bf1d28e35bec40d
Juan Camilo Avila Laverde <juankmilo1102@hotmail.com>
2022-06-29 23:22:36 -0500

    GUI file created, and Tk window created

---
+++ GUI_password_generator.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+# Variables
+window = Tk()
+window.title("Password Generator")
+window.geometry("780x640")
+window.minsize(400, 400)
+window.configure(bg="grey")
+
+window.mainloop()
