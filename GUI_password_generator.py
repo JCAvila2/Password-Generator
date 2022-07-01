378df2b110d2291439811870d2b896757e019287
Juan Camilo Avila Laverde <juankmilo1102@hotmail.com>
2022-07-01 12:45:32 -0500

    The generate button show the generated password in the password_label

--- GUI_password_generator.py
+++ GUI_password_generator.py
@@ -23,11 +23,12 @@ label_font = font.Font(family = 'Poppins', size = 15, weight = 'bold')
 buttons_font = font.Font(family = 'Roboto', size = 15, weight = 'bold')
 random_button = Button()
 chosen_characters_button = Button()
+password_label = Label()
 
 
 # Functions
-def generate_random_char(characters):
-    return random.choice(characters)
+def generate_random_char(list_of_characters): # Function that returns a random character from a list
+    return random.choice(list_of_characters)
 
 def generate_random_number(): # Function that returns a random number
     return random.randint(0, 9)
@@ -36,7 +37,7 @@ def add_number_to_password(total_len): # Function that returns a random number b
     return random.randint(0, total_len)
 
 
-def generate_without_input_characters(password_letters, password_numbers):
+def generate_without_input_characters(password_letters, password_numbers): # Function to generate a password without determinate characters
     password_length = password_letters + password_numbers
     password = str()
     print("Amount of numbers:", password_numbers)
@@ -50,6 +51,16 @@ def generate_without_input_characters(password_letters, password_numbers):
     return password
 
 
+def generate_with_input_characters(input_characters): # Function to generate a password with determinate characters
+    input_characters = list(input_characters)
+    password = ""
+    for i in range(len(input_characters)):
+        char = generate_random_char(input_characters)
+        input_characters.remove(char)
+        password += char
+    return password
+
+
 # Funtions for buttons
 def add(unit, max, addition_amount, number_type): # Function to add
     actualValue = number_type(unit.get())
@@ -59,6 +70,7 @@ def add(unit, max, addition_amount, number_type): # Function to add
         actualValue = 0
     unit.delete(0, 'end')
     unit.insert(END, number_type(actualValue))
+
 
 def sub(unit, max, addition_amount, number_type): # Function to subtract 
     actualValue = number_type(unit.get())
@@ -73,13 +85,13 @@ def sub(unit, max, addition_amount, number_type): # Function to subtract
 def generate_password(pool_of_characters, amount_of_letters, amount_of_numbers):
     print(pool_of_characters, amount_of_letters, amount_of_numbers)
     if pool_of_characters == "Random":
-        print(generate_without_input_characters(int(amount_of_letters), int(amount_of_numbers)))
+        password_generated = generate_without_input_characters(int(amount_of_letters), int(amount_of_numbers))
+        password_label.configure(text = password_generated)
+        print(password_generated)
 
     
 
-
-
-# Fix variables name
+# Generate password with random characters
 random_frame = Frame(window, height = 500, width = 500, bg = gray)
 
 amount_of_letters_label = Label(random_frame, text = "Amount of letters:", bg = gray, font = label_font, foreground = letters_color)
@@ -95,6 +107,7 @@ amount_of_numbers_entry.insert(END, "0")
 button_amount_of_numbers_sub = Button(random_frame, command = lambda : sub(amount_of_numbers_entry, 99, 1, int), text = "-", font = buttons_font, highlightthickness = 0, bd = 0, bg = gray, foreground = yellow) 
 
 button_generate_random = Button(random_frame, command = lambda : generate_password("Random", amount_of_letters_entry.get(), amount_of_numbers_entry.get()), text = "Generate", font = buttons_font, highlightthickness = 0, bd = 0, bg = gray, foreground = yellow)
+password_label = Label(random_frame, text = "", bg = gray, foreground = yellow, font = answer_font)
 
 def random_frame_show(): # Place the elements in the window
     amount_of_letters_label.grid(row = 2, column = 0)
@@ -107,6 +120,7 @@ def random_frame_show(): # Place the elements in the window
     amount_of_numbers_entry.grid(row = 2, column = 3, padx = 5, pady = 5)
     button_amount_of_numbers_sub.grid(row = 3, column = 3)
 
+    password_label.grid(row = 5, column = 0, columnspan = 4, pady = 5)
     button_generate_random.grid(row = 4, column = 0, pady = 20, columnspan = 4) 
     random_frame.place(relx = 0.5, rely = 0.5, anchor = "center")
 
@@ -117,11 +131,12 @@ def random_frame_hide(): # Hide the elements in the window
 
 def onCheck(option):
     if option == "Random":
+        print("Selected random")
         random_button.config(bg = yellow, font = buttons_font, foreground = "black")
         chosen_characters_button.config(bg = gray, font = buttons_font, foreground = letters_color)
         random_frame_show()
     elif option == "Chosen characters":
-        print("Selected ")
+        print("Selected Chosen characters")
         random_button.config(bg = gray, font = buttons_font, foreground = letters_color)
         chosen_characters_button.config(bg = yellow, font = buttons_font, foreground = "black")
         random_frame_hide()
@@ -137,22 +152,4 @@ chosen_characters_button.place(relx = 0.7, rely = 0.1, anchor = "center")
 
 
 
-
-
-
-
-
-
-
-
 window.mainloop()
-"""
-pool = input("password test: ")
-pool = list(pool)
-password = ""
-for i in range(len(pool)):
-    char = generate_random_char(pool)
-    pool.remove(char)
-    password += char
-print(password)
-"""
